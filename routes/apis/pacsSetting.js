c31603e85522fa2056eda028edfab40d3fbbaa59
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-06-06 23:28:22 +0800

    fix:新增sort api

--- routes/apis/pacsSetting.js
+++ routes/apis/pacsSetting.js
@@ -9,7 +9,8 @@ const DICOM = require('../../models/dicom')
 router.route('/').get(async (req, res) => {
     try {
         const data = await DICOM.find()
-        return res.status(200).json({ results: data, count: data.length })
+        const results = data.sort((a, b) => a.weights - b.weights)
+        return res.status(200).json({ results, count: results.length })
     } catch (e) {
         return res.status(500).json({ message: e.message })
     }
@@ -20,6 +21,24 @@ router.route('/').post(async (req, res) => {
         let dicom = new DICOM(req.body)
         dicom = await dicom.save()
         return res.status(200).json(dicom)
+    } catch (e) {
+        return res.status(500).json({ message: e.message })
+    }
+})
+
+router.route('/sort').patch(async (req, res) => {
+    try {
+        const data = req.body
+        for (let i = 0; i < data.length; i++) {
+            const dicom = await DICOM.findById(data[i]._id)
+            if (!dicom) return res.status(404).json({ message: 'Data not found.' })
+            Object.assign(dicom, data[i])
+            await dicom.save()
+        }
+
+        const dicomDatas = await DICOM.find()
+        const results = dicomDatas.sort((a, b) => a.weights - b.weights)
+        return res.status(200).json({ results, count: dicomDatas.length })
     } catch (e) {
         return res.status(500).json({ message: e.message })
     }
@@ -42,7 +61,7 @@ router.route('/:id').delete(async (req, res) => {
         const dicom = await DICOM.findById(req.params.id)
         if (!dicom) return res.status(404).json({ message: 'Data not found.' })
         await dicom.remove()
-        return res.status(200).json({ message: 'Data deleted.' })
+        return res.status(200).json(dicom)
     } catch (e) {
         return res.status(500).json({ message: e.message })
     }
