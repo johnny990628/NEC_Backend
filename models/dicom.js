aae736f39de861b6a665ec944ef4d12663fd2645
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-06-08 15:08:13 +0800

    fix:修正dicom Schema

--- models/dicom.js
+++ models/dicom.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const dicomSchema = new Schema({
+    shorteningPacsName: { type: String, required: true },
     pacsURL: { type: String, required: true },
     pacsName: { type: String, required: true },
-    pacsPort: { type: String, required: true },
-    pacsAETitle: { type: String, required: true },
-    pacsWadoURI: { type: String, required: true },
-    pacsWadoPort: { type: String, required: true },
-    pacsWadoAETitle: { type: String, required: true },
     pacsWadoURL: { type: String, required: true },
     isOpen: { type: Boolean, required: true },
     weights: { type: Number, required: true },
